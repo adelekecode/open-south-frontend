fab58306bac6d8a17ef06890ee8ae349098a5ded
henzyd <henzydee@gmail.com>
2023-11-10 16:20:22 +0100

    created app navbar

--- src/components/search-field.tsx
+++ src/components/search-field.tsx
@@ -1,8 +1,51 @@
-import { OutlinedInput } from "@mui/material";
-import { OutlinedInputProps } from "@mui/material";
+import { OutlinedInput, OutlinedInputProps } from "@mui/material";
+import { FiSearch } from "react-icons/fi";
+import { twMerge } from "tailwind-merge";
 
-type SearchFieldProps = OutlinedInputProps;
+type SearchFieldProps = OutlinedInputProps & {
+  wrapperClassName?: string;
+  onSearch?: () => void;
+};
 
-export default function SearchField({ ...props }: SearchFieldProps) {
-  return <OutlinedInput type="search" {...props} />;
+export default function SearchField({
+  onSearch,
+  wrapperClassName = "",
+  sx,
+  className,
+  ...props
+}: SearchFieldProps) {
+  return (
+    <div className={`flex rounded-t-[4px] overflow-hidden ${wrapperClassName}`}>
+      <OutlinedInput
+        className={twMerge(``, className)}
+        sx={{
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderTop: "0px",
+            borderRight: "0px",
+            borderLeft: "0px",
+            borderWidth: "2px",
+            borderColor: "#0e82bb !important",
+            borderRadius: "4px 0 0 0",
+          },
+          "& .MuiOutlinedInput-input": {
+            padding: "7px 12px",
+            fontFamily: '"Work Sans", sans-serif',
+            backgroundColor: "rgb(228 228 231)",
+            "&::placeholder": {
+              fontStyle: "italic",
+            },
+          },
+          ...sx,
+        }}
+        type="search"
+        {...props}
+      />
+      <button
+        className="bg-primary-600 p-1 px-2 hover:opacity-50 transition-all outline-0"
+        onClick={onSearch}
+      >
+        <FiSearch className="w-5 h-5 text-white" />
+      </button>
+    </div>
+  );
 }
--- src/layouts/app/header.tsx
+++ src/layouts/app/header.tsx
@@ -1,4 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
+import { BiLock } from "react-icons/bi";
+import { IoPersonCircleOutline } from "react-icons/io5";
 import { twMerge } from "tailwind-merge";
 import Logo from "~/components/logo";
 import SearchField from "~/components/search-field";
@@ -25,12 +27,24 @@ export default function Header() {
 
   return (
     <nav className="w-full shadow-appNavBar flex flex-col items-center">
-      <div className="border-b-[1.5px] border-zinc-200 w-full max-w-maxAppWidth flex items-center justify-between gap-4 p-4 px-6">
-        <Link to={"/"} className="p-6 w-fit hover:bg-zinc-100">
-          <Logo className="w-[10rem]" />
-        </Link>
-        <div>
-          <SearchField />
+      <div className="w-full border-b-[1.5px] border-zinc-200 flex items-center justify-center">
+        <div className=" w-full max-w-maxAppWidth flex items-center justify-between gap-4 p-4 px-6">
+          <Link to={"/"} className="p-6 w-fit hover:bg-zinc-100">
+            <Logo className="w-[10rem]" />
+          </Link>
+          <div className="flex flex-col items-end gap-4">
+            <div className="flex item-center gap-4 [&_a]:rounded-full [&_a>p]:text-primary-700 [&_a]:p-2 [&_a]:py-1 [&_a>p]:text-sm [&_a>p]:font-medium [&_a]:flex [&_a]:items-center [&_a]:gap-2">
+              <Link to="/login" className="hover:bg-zinc-100">
+                <BiLock className="text-primary-700" />
+                <p>To log in</p>
+              </Link>
+              <Link to="/signup" className="hover:bg-zinc-100">
+                <IoPersonCircleOutline className="text-primary-700" />
+                <p>Signup</p>
+              </Link>
+            </div>
+            <SearchField placeholder="Research" className="w-[300px]" />
+          </div>
         </div>
       </div>
       <div className="px-6 flex items-start w-full max-w-maxAppWidth">
