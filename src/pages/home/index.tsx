bbf70ffddffe366c1b807adeb9f0a25105469b1b
henzyd <henzydee@gmail.com>
2023-11-24 15:46:28 +0100

    created category section in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import Seo from "~/components/seo";
 import Panel from "./panel";
 import Banner from "./banner";
 import Dataset from "./dataset";
+import Category from "./category";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <main>
         <Panel />
         <Dataset />
+        <Category />
         <Banner />
       </main>
     </>
