7727cf1ad49f5ac51bd02947c8a06f0cc5364b91
henzyd <henzydee@gmail.com>
2023-12-04 19:14:51 +0100

    made header responsive

--- src/layouts/app/header.tsx
+++ src/layouts/app/header.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
+import { IconButton } from "@mui/material";
 import { BiLock } from "react-icons/bi";
-import { IoPersonCircleOutline, IoLogOutOutline, IoSettingsOutline } from "react-icons/io5";
+import { IoPersonCircleOutline, IoLogOutOutline, IoMenu, IoSettingsOutline } from "react-icons/io5";
 import { twMerge } from "tailwind-merge";
 import Logo from "~/components/logo";
 import SearchInput from "~/components/search-input";
@@ -10,37 +11,12 @@ import CurrentUserAvatar from "~/components/current-user-avatar";
 import useAppStore from "~/store/app";
 
 type HeaderProps = {
+  routes: { to: string; name: string }[];
   setRoutePath: (data: React.ReactNode[]) => void;
+  setOpenSidebar: (bool: boolean) => void;
 };
 
-const routes = [
-  {
-    to: "/datasets",
-    name: "Datasets",
-  },
-  {
-    to: "/categories",
-    name: "Categories",
-  },
-  {
-    to: "/organizations",
-    name: "Organizations",
-  },
-  {
-    to: "/about",
-    name: "About Us",
-  },
-  {
-    to: "/news",
-    name: "News",
-  },
-  {
-    to: "/contact",
-    name: "Contact Us",
-  },
-];
-
-export default function Header({ setRoutePath }: HeaderProps) {
+export default function Header({ routes, setRoutePath, setOpenSidebar }: HeaderProps) {
   const navigate = useNavigate();
   const { pathname, state } = useLocation();
 
@@ -77,13 +53,21 @@ export default function Header({ setRoutePath }: HeaderProps) {
 
   return (
     <nav className="w-full shadow-appNavBar flex flex-col items-center">
-      <div className="w-full border-b-[1.5px] border-zinc-200 flex items-center justify-center">
-        <div className=" w-full max-w-maxAppWidth flex items-start justify-between gap-4 p-4 px-6 tablet:pl-2">
+      <div className="w-full border-b-[1.5px] border-zinc-200 flex items-center tablet:pb-2 justify-center">
+        <div className=" w-full max-w-maxAppWidth flex items-start justify-between gap-4 p-4 px-6 tablet:pl-2 largeMobile:pr-4 largeMobile:!pl-0 tablet:grid tablet:grid-cols-[1fr,80px]">
           <Link to={"/"} className="p-6 px-4 w-fit hover:bg-zinc-100">
-            <Logo className="w-[10rem]" />
+            <Logo className="w-[10rem] tablet:w-[8rem] mediumMobile:!w-[7rem]" />
           </Link>
-          <div className="flex flex-col items-end gap-4 pr-4 tablet:pr-0">
-            <div className="flex item-center gap-4 flex-wrap [&_button]:rounded-full [&_button>p]:text-primary-700 [&_button]:p-2 [&_button]:py-1 [&_button>p]:text-sm [&_button>p]:font-medium [&_button]:flex [&_button]:items-center [&_button]:gap-2">
+          <IconButton
+            className="w-fit !ml-auto !hidden tablet:!block"
+            onClick={() => {
+              setOpenSidebar(true);
+            }}
+          >
+            <IoMenu className="text-zinc-800" />
+          </IconButton>
+          <div className="flex flex-col items-end gap-4 pr-4 tablet:pr-0 tablet:w-full tablet:col-span-2">
+            <div className="flex item-center gap-4 flex-wrap [&_button]:rounded-full [&_button>p]:text-primary-700 [&_button]:p-2 [&_button]:py-1 [&_button>p]:text-sm [&_button>p]:font-medium [&_button]:flex [&_button]:items-center [&_button]:gap-2 tablet:hidden">
               {currentUser ? (
                 <>
                   <div className="flex items-center gap-3">
@@ -151,11 +135,15 @@ export default function Header({ setRoutePath }: HeaderProps) {
                 </>
               )}
             </div>
-            <SearchInput placeholder="Search" className="w-[300px]" />
+            <SearchInput
+              placeholder="Search"
+              className="w-[300px] tablet:w-full"
+              wrapperClassName="tablet:w-full tablet:pl-4"
+            />
           </div>
         </div>
       </div>
-      <div className="px-6 tablet:px-2 flex items-start w-full max-w-maxAppWidth">
+      <div className="px-6 tablet:px-2 flex items-start w-full max-w-maxAppWidth tablet:hidden">
         {routes.map((item, index) => (
           <NavLink
             to={item.to}
