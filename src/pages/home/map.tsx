11ba0a998ffd223431b2828d1afee25dcd130714
henzyd <henzydee@gmail.com>
2023-12-07 18:40:25 +0100

    chore: add dummy data to map

--- src/pages/home/map.tsx
+++ src/pages/home/map.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import data from "~/utils/data/map.json";
 
 export default function Map() {
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    // (async () => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOXGL_ACCESS_TOKEN;
 
     const map = new mapboxgl.Map({
@@ -19,46 +19,47 @@ export default function Map() {
 
     map.addControl(new mapboxgl.NavigationControl());
 
-    // map.on("load", () => {
-    //   map.addSource("earthquakes", {
-    //     type: "geojson",
-    //     // data: "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
-    //     data: "",
-    //     cluster: true,
-    //     clusterMaxZoom: 14,
-    //     clusterRadius: 50,
-    //   });
+    map.on("load", () => {
+      map.addSource("earthquakes", {
+        type: "geojson",
+        // data: "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
+        data: data as any,
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50,
+      });
 
-    //   map.addLayer({
-    //     id: "clusters",
-    //     type: "circle",
-    //     source: "earthquakes",
-    //     filter: ["has", "point_count"],
-    //     paint: {
-    //       "circle-color": [
-    //         "step",
-    //         ["get", "point_count"],
-    //         "#51bbd6",
-    //         100,
-    //         "#f1f075",
-    //         750,
-    //         "#f28cb1",
-    //       ],
-    //       "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
-    //     },
-    //   });
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "earthquakes",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1",
+          ],
+          "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+        },
+      });
 
-    //   map.addLayer({
-    //     id: "cluster-count",
-    //     type: "symbol",
-    //     source: "earthquakes",
-    //     filter: ["has", "point_count"],
-    //     layout: {
-    //       "text-field": ["get", "point_count_abbreviated"],
-    //       "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-    //       "text-size": 12,
-    //     },
-    //   });
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "earthquakes",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": ["get", "point_count_abbreviated"],
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+        },
+      });
+    });
 
     //   // map.addLayer({
     //   //   id: "unclustered-point",
@@ -73,8 +74,6 @@ export default function Map() {
     //   //   },
     //   // });
     // });
-
-    // })();
 
     // const mapData: {
     //   type: string;
@@ -124,6 +123,35 @@ export default function Map() {
       map.remove();
     };
   }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await fetch(
+  //       "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+  //         encodeURIComponent("Brazil") +
+  //         ".json?access_token=" +
+  //         "pk.eyJ1IjoiaGVuenlkIiwiYSI6ImNscGFoMjhhbzA4YjIya3FyZ2k1dHVvY3cifQ.BXdS3AGFaYxLJGU928a_5g"
+  //     );
+  //     const data = await response.json();
+
+  //     if (data.features.length > 0) {
+  //       const coordinates = data.features[0].center;
+
+  //       const _mapData = {
+  //         type: "Feature",
+  //         geometry: {
+  //           type: "Point",
+  //           coordinates,
+  //         },
+  //         properties: {
+  //           country: "Nigeria",
+  //         },
+  //       };
+
+  //       console.log(_mapData);
+  //     }
+  //   })();
+  // }, []);
 
   return (
     <div className="w-full">
