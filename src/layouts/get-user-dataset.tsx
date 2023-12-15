e156755ba527421bad3e090cbb1a591031f110ba
henzyd <henzydee@gmail.com>
2023-12-15 11:56:01 +0100

    saving changes

--- src/layouts/get-user-dataset.tsx
+++ src/layouts/get-user-dataset.tsx
@@ -3,7 +3,7 @@ import DashboardLoader from "~/components/loader/dashboard-loader";
 import NotFound from "~/pages/404";
 import { useDatasetDetails } from "~/queries/dataset";
 
-export default function GetDataset() {
+export default function GetUserDataset() {
   const { id } = useParams();
   const { data, isLoading } = useDatasetDetails(id || "");
 
