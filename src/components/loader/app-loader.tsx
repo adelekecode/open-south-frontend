b145cf241742a631b6a2e09b78cd2152cbaefcbc
henzyd <henzydee@gmail.com>
2023-12-20 20:38:48 +0100

    refactor: changed app loader to a spinner

--- src/components/loader/app-loader.tsx
+++ src/components/loader/app-loader.tsx
@@ -1,11 +1,14 @@
 export default function AppLoader() {
   return (
-    <div className="grid grid-rows-[60px,1fr] gap-6 p-6 h-screen">
-      <div className="animate-pulse rounded-lg bg-gray-200" />
-      <div className="grid grid-cols-[200px,1fr] gap-6">
-        <div className="animate-pulse rounded-lg h-full bg-gray-200" />
-        <div className="animate-pulse rounded-lg h-full bg-gray-200" />
-      </div>
+    // <div className="grid grid-rows-[60px,1fr] gap-6 p-6 h-screen">
+    //   <div className="animate-pulse rounded-lg bg-gray-200" />
+    //   <div className="grid grid-cols-[200px,1fr] gap-6">
+    //     <div className="animate-pulse rounded-lg h-full bg-gray-200" />
+    //     <div className="animate-pulse rounded-lg h-full bg-gray-200" />
+    //   </div>
+    // </div>
+    <div className="flex items-center justify-center w-full h-screen">
+      <span className="appLoader"></span>
     </div>
   );
 }
