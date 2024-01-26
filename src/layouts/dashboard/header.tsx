6510da412b74b8a0faba200d16c30793786d95d3
henzyd <henzydee@gmail.com>
2024-01-26 16:49:52 +0100

    chore: added profile popup

--- src/layouts/dashboard/header.tsx
+++ src/layouts/dashboard/header.tsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ClickAwayListener, Fade, IconButton, Paper, Popper } from "@mui/material";
 import { IoMdAdd } from "react-icons/io";
+import { FaAngleDown } from "react-icons/fa6";
 import CurrentUserAvatar from "~/components/current-user-avatar";
 import { useCurrentUser } from "~/queries/user";
+import useAppStore from "~/store/app";
 
 export default function Header() {
   const navigate = useNavigate();
 
+  const { setDisplayLogoutModal } = useAppStore();
+
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [profileAnchorEl, setProfileAnchorEl] = useState<HTMLButtonElement | null>(null);
 
   const dropdownDisplay = Boolean(anchorEl);
+
+  const profileDropdownDisplay = Boolean(profileAnchorEl);
 
   const { data: currentUser } = useCurrentUser(undefined, {
     enabled: false,
@@ -62,20 +69,75 @@ export default function Header() {
         </span>
       )}
 
-      <button
-        className="flex items-center gap-3"
-        onClick={() => {
-          navigate(currentUser?.role === "admin" ? "/admin/profile" : "/account/profile");
-        }}
-      >
-        <CurrentUserAvatar />
-        <p className="flex items-center gap-1 capitalize text-sm font-medium largeMobile:hidden">
-          <span>
-            {currentUser?.first_name || currentUser?.profile_data?.first_name || "-------"}
-          </span>{" "}
-          <span>{currentUser?.last_name || currentUser?.profile_data?.last_name || "-------"}</span>
-        </p>
-      </button>
+      <ClickAwayListener onClickAway={() => setProfileAnchorEl(null)}>
+        <div>
+          <button
+            className="flex items-center gap-3"
+            onClick={(e) => setProfileAnchorEl(profileAnchorEl ? null : e.currentTarget)}
+          >
+            <CurrentUserAvatar />
+            <FaAngleDown className="text-info-900" />
+          </button>
+          <Popper
+            transition
+            open={profileDropdownDisplay}
+            anchorEl={profileAnchorEl}
+            className="z-[100] !mt-2"
+          >
+            {({ TransitionProps }) => (
+              <Fade {...TransitionProps} timeout={200}>
+                <Paper className="flex flex-col overflow-hidden relative !shadow min-w-[150px]">
+                  <div className="p-3 py-3 border-b flex flex-col justify-center items-center gap-2">
+                    <CurrentUserAvatar
+                      className="!border !w-fit !p-1"
+                      sx={{
+                        width: 200,
+                        height: 70,
+                      }}
+                    />
+                    <p className="text-xs flex items-center gap-1">
+                      <span>
+                        {currentUser?.first_name ||
+                          currentUser?.profile_data?.first_name ||
+                          "-------"}
+                      </span>
+                      <span>
+                        {currentUser?.last_name ||
+                          currentUser?.profile_data?.last_name ||
+                          "-------"}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="flex flex-col divide-y">
+                    <button
+                      className=" text-sm p-4 py-2 hover:bg-info-50"
+                      onClick={() => {
+                        navigate(
+                          currentUser?.role === "admin" ? "/admin/profile" : "/account/profile"
+                        );
+
+                        return setProfileAnchorEl(null);
+                      }}
+                    >
+                      Profile
+                    </button>
+                    <button
+                      className=" text-sm p-4 py-2 hover:bg-info-50"
+                      onClick={() => {
+                        setDisplayLogoutModal(true);
+
+                        return setProfileAnchorEl(null);
+                      }}
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </Paper>
+              </Fade>
+            )}
+          </Popper>
+        </div>
+      </ClickAwayListener>
     </nav>
   );
 }
