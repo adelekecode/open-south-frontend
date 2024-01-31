e14be8640de8e897991a883bfdcd823eb56bb9e8
henzyd <henzydee@gmail.com>
2024-01-31 12:11:04 +0100

    chore: implemented file preview for csv

--- src/pages/dataset-details/file-preview.tsx
+++ src/pages/dataset-details/file-preview.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import * as XLSX from "xlsx";
+// import * as XLSX from "xlsx";
+import * as Papa from "papaparse";
 import DataGrid from "~/components/data-grid";
 import Modal from "~/components/modal";
 import { usePublicFilePreview } from "~/queries/dataset";
+import Button from "~/components/button";
 
 type FilePreviewProps = {
   open: boolean;
@@ -11,7 +13,8 @@ type FilePreviewProps = {
 };
 
 export default function FilePreview({ open, setOpen, file }: FilePreviewProps) {
-  const [excelData, setExcelData] = useState<any[]>([]);
+  const [excelData] = useState<any[] | null>(null);
+  const [csvData, setCsvData] = useState<any[] | null>(null);
 
   const { data, isLoading } = usePublicFilePreview(file?.file_url || "", {
     enabled: !!file?.file_url,
@@ -19,22 +22,76 @@ export default function FilePreview({ open, setOpen, file }: FilePreviewProps) {
 
   useEffect(() => {
     if (data) {
-      const workbook = XLSX.read(data, { type: "array" });
+      // const workbook = XLSX.read(data, { type: "array" });
 
-      const sheetName = workbook.SheetNames[0];
+      // // console.log(workbook, "workbook");
 
-      setExcelData(XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]));
+      // const sheetName = workbook.SheetNames[0];
+
+      // // console.log(sheetName, "sheetName");
+
+      // const res = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+      // // console.log(res, "excelData");
+
+      // const workbook = XLSX.read(data, { type: "array" });
+
+      // console.log(workbook);
+
+      // const sheetName = workbook.SheetNames[0];
+
+      // if (!sheetName) {
+      //   throw new Error("No sheet found in the workbook.");
+      // }
+
+      // const sheet = workbook.Sheets[sheetName];
+
+      // console.log(sheet, "sheet");
+
+      // if (!sheet) {
+      //   throw new Error(`Sheet "${sheetName}" not found in the workbook.`);
+      // }
+
+      // const res = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+      // console.log("Excel data:", res);
+
+      // setExcelData(res);
+      // if (file?.format === "xlsx") {
+      // } else
+      if (file?.format === "text/csv") {
+        Papa.parse(data as any, {
+          header: true,
+          dynamicTyping: true,
+          skipEmptyLines: true,
+          complete: (result) => {
+            const data = (result.data as any[]).map((item, index) => ({
+              id: index,
+
+              ...item,
+            }));
+
+            setCsvData(data);
+          },
+          error: (err) => {
+            console.error("Error parsing CSV:", err);
+          },
+        });
+      }
     }
-  }, [data]);
+  }, [data, file?.format]);
 
   if (!file) {
     return;
   }
 
-  const columns =
-    excelData.length > 0
+  const csvColumns =
+    csvData && csvData.length > 0
+      ? Object.keys(csvData[0]).map((key) => ({ field: key, headerName: key }))
+      : null;
+  const excelColumns =
+    excelData && excelData.length > 0
       ? Object.keys(excelData[0]).map((key) => ({ field: key, headerName: key }))
-      : [];
+      : null;
 
   return (
     <Modal
@@ -48,7 +105,7 @@ export default function FilePreview({ open, setOpen, file }: FilePreviewProps) {
         className: "px-4",
       }}
       innerContainer={{
-        className: "pt-[2rem] w-[800px] tablet:!w-full",
+        className: "w-[800px] tablet:!w-full pt-16",
       }}
     >
       <div className="flex flex-col gap-4 w-full">
@@ -59,8 +116,8 @@ export default function FilePreview({ open, setOpen, file }: FilePreviewProps) {
               <div className="animate-pulse bg-gray-200 h-10 rounded-t-sm" />
               <div className="flex flex-col gap-2">
                 {Array.from({ length: 8 }).map((_, index) => (
-                  <div className={`${index < 7 && "border-b pb-2"}`}>
-                    <div key={index + 1} className={`animate-pulse bg-gray-200 h-8`} />
+                  <div className={`${index < 7 && "border-b pb-2"}`} key={index + 1}>
+                    <div className={`animate-pulse bg-gray-200 h-8`} />
                   </div>
                 ))}
               </div>
@@ -68,10 +125,70 @@ export default function FilePreview({ open, setOpen, file }: FilePreviewProps) {
           </div>
         ) : (
           <>
-            <header className="flex flex-col gap-1">
-              <h1 className="text-lg font-medium">{"File name"}</h1>
+            <header className="flex items-center justify-between gap-1">
+              <h1 className="text-lg font-medium">
+                {file.file_name
+                  ? file.file_name[0].toUpperCase() + file.file_name.slice(1)
+                  : "-------"}
+              </h1>
+              <Button
+                color="info"
+                variant="outlined"
+                className="!p-2 !px-4 !text-xs"
+                onClick={() => {
+                  if (
+                    file?.format === "xlsx" ||
+                    file?.format ===
+                      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                  ) {
+                    const blob = new Blob([data as BlobPart], {
+                      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    });
+
+                    const url = URL.createObjectURL(blob);
+
+                    const link = document.createElement("a");
+
+                    link.href = url;
+
+                    link.download = `${file?.file_name || "example"}.xlsx`;
+
+                    document.body.appendChild(link);
+
+                    link.click();
+
+                    document.body.removeChild(link);
+
+                    URL.revokeObjectURL(url);
+                  } else if (file?.format === "text/csv") {
+                    // Assuming `data` is a valid CSV file content
+                    const blob = new Blob([data as BlobPart], { type: "text/csv" });
+
+                    const url = URL.createObjectURL(blob);
+
+                    const link = document.createElement("a");
+
+                    link.href = url;
+
+                    link.download = `${file?.file_name || "example"}.csv`;
+
+                    document.body.appendChild(link);
+
+                    link.click();
+
+                    document.body.removeChild(link);
+
+                    URL.revokeObjectURL(url);
+                  }
+                }}
+              >
+                Download
+              </Button>
             </header>
-            <DataGrid rows={excelData} columns={columns} />
+            <DataGrid
+              rows={csvData || excelData || []}
+              columns={csvColumns || excelColumns || []}
+            />
           </>
         )}
       </div>
