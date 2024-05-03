f3e1e3b63d25c483f33c2436f8186dc5d485c2dd
henzyd <henzydee@gmail.com>
2024-05-03 14:48:11 +0100

    chore: translated news page

--- src/layouts/app/index.tsx
+++ src/layouts/app/index.tsx
@@ -68,11 +68,11 @@ export default function AppLayout() {
   const breadcrumbs = useMemo(
     () => [
       <Link key="1" to="/" className="hover:underline">
-        Welcome
+        {t("main.breadcrumbs")}
       </Link>,
       ...routePath,
     ],
-    [routePath]
+    [routePath, t]
   );
 
   return (
