d173b61b631c50bc40a6d4659c2adc607e7a35ac
henzyd <henzydee@gmail.com>
2024-06-07 06:35:20 +0100

    made translation dynamic

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,12 +18,9 @@ export default function About() {
       <main className="w-full mx-auto max-w-maxAppWidth p-6 px-10 tablet:px-5 largeMobile:!px-4 pt-0 pb-8 flex flex-col gap-6">
         <h1 className="text-3xl largeMobile:!text-2xl font-semibold">{t("title", option)}</h1>
         <div className="flex flex-col gap-4">
-          <p>{t("body.1", option)}</p>
-          <p>{t("body.2", option)}</p>
-          <p>{t("body.3", option)}</p>
-          <p>{t("body.4", option)}</p>
-          <p>{t("body.5", option)}</p>
-          <p>{t("body.6", option)}</p>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <p key={index + 1}>{t(`body.${index + 1}`, option)}</p>
+          ))}
           <p className="mt-4 text-lg tablet:text-base largeMobile:!text-sm">
             <span className="font-medium">{t("body.footnote.welcome", option)}</span> –{" "}
             <span className="font-Marhey">{t("body.footnote.extra", option)}</span>
