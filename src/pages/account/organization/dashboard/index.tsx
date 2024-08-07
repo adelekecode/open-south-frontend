48183fa2fdbec8c66d456b4e3d023c5150e972c9
henzyd <henzydee@gmail.com>
2024-08-07 21:09:33 +0100

    refactor: organization dashbard

--- src/pages/account/organization/dashboard/index.tsx
+++ src/pages/account/organization/dashboard/index.tsx
@@ -1,8 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 import { GridColDef } from "@mui/x-data-grid";
-import { twMerge } from "tailwind-merge";
-import moment from "moment";
+import { useTranslation } from "react-i18next";
 import { IoCloudDownloadOutline, IoGridOutline } from "react-icons/io5";
 import { AiOutlineEye } from "react-icons/ai";
 import DataGrid from "~/components/data-grid";
@@ -10,150 +9,87 @@ import Button from "~/components/button";
 import { useUserOrganizationDatasets } from "~/queries/dataset";
 import MostAccessedDatasets from "./most-accessed-datasets";
 import TopTrafficLocations from "./top-traffic-locations";
-
-const columns: GridColDef[] = [
-  {
-    field: "id",
-    headerName: "NO.",
-    minWidth: 10,
-    renderCell: ({ api, row }) => {
-      const { getAllRowIds } = api;
-
-      return getAllRowIds().indexOf(row.id) + 1;
-    },
-  },
-  {
-    field: "title",
-    headerName: "TITLE",
-    flex: 1,
-    minWidth: 200,
-  },
-  {
-    field: "created_at",
-    headerName: "CREATED AT",
-    flex: 1,
-    minWidth: 150,
-    valueFormatter: ({ value }) => {
-      return moment(value).format("Do MMM, YYYY");
-    },
-    sortComparator: (v1, v2) => {
-      return new Date(v1).getTime() - new Date(v2).getTime();
-    },
-    align: "center",
-    headerAlign: "center",
-  },
-  {
-    field: "updated_at",
-    headerName: "UPDATED AT",
-    minWidth: 150,
-    flex: 1,
-    valueFormatter: ({ value }) => {
-      return moment(value).fromNow();
-    },
-    sortComparator: (v1, v2) => {
-      return new Date(v1).getTime() - new Date(v2).getTime();
-    },
-    align: "center",
-    headerAlign: "center",
-  },
-  {
-    field: "views",
-    headerName: "VIEWS",
-    flex: 1,
-    minWidth: 70,
-    valueFormatter: ({ value }) => {
-      return value.count;
-    },
-    sortComparator: (v1, v2) => {
-      return v1 - v2.count;
-    },
-    align: "center",
-    headerAlign: "center",
-  },
-  {
-    field: "files_count",
-    headerName: "FILES",
-    minWidth: 70,
-    valueFormatter: ({ value }) => {
-      return value;
-    },
-    sortComparator: (v1, v2) => {
-      return v1 - v2;
-    },
-    align: "center",
-    headerAlign: "center",
-  },
-  {
-    field: "status",
-    headerName: "Status",
-    flex: 1,
-    renderCell: ({ value }) => {
-      const obj: {
-        element: any;
-        styles: string;
-      } = {
-        element: "-------",
-        styles: "py-1 px-2 rounded-full text-xs",
-      };
-
-      if (value === "pending") {
-        obj.element = (
-          <p className={twMerge(obj.styles, `text-orange-500 border border-orange-500`)}>Pending</p>
-        );
-      } else if (value === "published") {
-        obj.element = (
-          <p className={twMerge(obj.styles, `text-green-500 border border-green-500`)}>Published</p>
-        );
-      } else if (value === "rejected") {
-        obj.element = (
-          <p className={twMerge(obj.styles, `text-red-500 border border-red-500`)}>Rejected</p>
-        );
-      } else if (value === "further_review") {
-        obj.element = (
-          <p className={twMerge(obj.styles, `text-info-800 border border-info-800`)}>
-            Further Review
-          </p>
-        );
-      }
-
-      return obj.element;
-    },
-    sortable: false,
-    minWidth: 130,
-    align: "center",
-    headerAlign: "center",
-  },
-];
+import {
+  createColumn,
+  createDatasetStatusColumn,
+  createDateColumn,
+  createIdColumn,
+} from "~/utils/table-helpers";
 
 export default function Dashboard() {
+  const { t } = useTranslation("dashboard-layout/account/organization/dashboard");
+
   const { slug } = useParams();
 
   const navigate = useNavigate();
 
   const queryClient = useQueryClient();
   const organization = queryClient.getQueryData<Organization>([`/organisations/${slug}/`]);
+  const currentUser = queryClient.getQueryData<CurrentUser>(["/auth/users/me/"]);
 
-  const { data: datasets, isLoading: isDatasetsLoading } = useUserOrganizationDatasets(
-    organization?.id || ""
-  );
+  const columns: GridColDef[] = [
+    createIdColumn(
+      {
+        page: 0,
+        pageSize: 10,
+      },
+      {
+        headerName: t("latest-dataset-created.table.header.no"),
+      }
+    ),
+    createColumn({
+      field: "views",
+      minWidth: 100,
+      headerName: t("latest-dataset-created.table.header.views"),
+    }),
+    createColumn({
+      field: "files_count",
+      headerName: t("latest-dataset-created.table.header.files"),
+      minWidth: 100,
+    }),
+    createDateColumn({
+      field: "created_at",
+      headerName: t("latest-dataset-created.table.header.created-at"),
+    }),
+    createDateColumn({
+      field: "updated_at",
+      headerName: t("latest-dataset-created.table.header.updated-at"),
+    }),
+    createDatasetStatusColumn({
+      field: "status",
+      headerName: t("latest-dataset-created.table.header.status"),
+    }),
+  ];
+
+  const { data: datasets, isLoading: isDatasetsLoading } = useUserOrganizationDatasets({
+    orgId: organization?.id || "",
+    searchParams: {
+      search: "",
+      status: "",
+      limit: "10",
+      offset: "0",
+    },
+  });
 
   return (
     <>
       <main className="p-6 px-8 pb-12 tablet:px-6 largeMobile:!px-4 gap-4 flex flex-col">
-        <div className="flex items-center justify-between gap-4">
+        <div className="flex justify-between items-center">
           <h1 className="text-xl font-medium flex items-center gap-2">
-            Dashboard <span className="text-sm">{">"}</span>{" "}
-            <span className="text-info-800">{organization?.name || "-----"}</span>
+            <span className="text-info-800 capitalize">{organization?.name || "-----"}</span>
+            <span className="text-sm">{">"}</span> <span className="text-sm">{t("title")}</span>
           </h1>
-          <Button
-            className="!py-2 !px-3 !text-xs"
-            variant="outlined"
-            onClick={() => {
-              navigate(`/account/${slug}/edit`);
-            }}
-          >
-            Edit
-          </Button>
+          {currentUser?.id === organization?.user && (
+            <Button
+              className="!py-2 !px-3 !text-xs"
+              variant="outlined"
+              onClick={() => {
+                navigate(`/account/${slug}/edit`);
+              }}
+            >
+              Edit
+            </Button>
+          )}
         </div>
         <div className="w-full border border-info-100 bg-white p-4 rounded-md">
           <div className="grid grid-cols-3 [@media(max-width:900px)]:grid-cols-2 [@media(max-width:650px)]:!grid-cols-1 gap-4   [&>div]:w-full [&>div]:rounded-md [&>div]:p-4 [&>div]:flex [&>div]:justify-between [&>div]:min-h-[9rem]   [&>div>div]:flex [&>div>div]:flex-col [&>div>div]:mt-2 [&>div>div]:gap-3   [&>div>div>p]:font-semibold [&>div>div>p]:text-sm [&>div>div>h1]:text-4xl [&>div>div>h1]:font-semibold">
